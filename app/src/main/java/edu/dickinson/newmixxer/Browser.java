67184be97d5cac0a947ba27efe4723636ace72d2
zolboo1024 <erdenebz@dickinson.edu>
2019-02-14 09:32:55 -0500

    Some more comments 01/31/2019

--- app/src/main/java/edu/dickinson/newmixxer/Browser.java
+++ app/src/main/java/edu/dickinson/newmixxer/Browser.java
@@ -3,6 +3,7 @@ package edu.dickinson.newmixxer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.HttpAuthHandler;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
@@ -13,7 +14,7 @@ import android.widget.Toast;
  * screen once login or signup is clicked.
  *
  * Currently uses WebView Class but there
- * might be something else better 
+ * might be something else better
  *
  */
 public class Browser extends AppCompatActivity {
@@ -26,6 +27,7 @@ public class Browser extends AppCompatActivity {
         webView = (WebView) findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setAppCacheEnabled(true);
+        webView.canGoBack();
         Intent intent= getIntent();
         url= intent.getStringExtra("url");
         callUrl(url);
@@ -33,7 +35,10 @@ public class Browser extends AppCompatActivity {
     protected void callUrl(String url){
         webView.loadUrl(url);
         webView.setWebViewClient(new WebViewClient(){
-
+            public void onReceivedHttpAuthRequest(WebView view,
+                                                  HttpAuthHandler handler, String host, String realm) {
+                view.getHttpAuthUsernamePassword("www.language-exchanges.org", "The Mixxer");
+            }
             public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
 
                 //Intent intent= new Intent(getApplicationContext(), MainActivity.class);
--- app/src/main/java/edu/dickinson/newmixxer/MainActivity.java
+++ app/src/main/java/edu/dickinson/newmixxer/MainActivity.java
@@ -1,8 +1,15 @@
 package edu.dickinson.newmixxer;
 
 import android.app.ProgressDialog;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.customtabs.CustomTabsClient;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.customtabs.CustomTabsServiceConnection;
+import android.support.customtabs.CustomTabsSession;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,6 +30,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Zolboo Erdenebaatar 11/19/2018
@@ -34,107 +43,71 @@ public class MainActivity extends AppCompatActivity {
     ProgressDialog pd; //opening up this Activity might take a while. So this is a dialog that spins while the app is loading
     Button signUp; //sign up Button, same as logIn
     Button logIn;
+    public static final String CUSTOM_TAB_PACKAGE_NAME = "com.android.chrome";
+    public static CustomTabsClient mCustomTabsClient;
+    public static CustomTabsSession mCustomTabsSession;
     //public static String joReturn;    //was useful when we used the method "yourDataTask". No longer useful
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        if(joReturn!=null && joReturn.length()>70){
-//            Intent browserIntent = new Intent(getApplicationContext(), Browser.class);
-//            browserIntent.putExtra("url", "https://mixxertest.dickinson.edu/user-search");
-//            startActivity(browserIntent);
-//        }
-            signUp = (Button) findViewById(R.id.signUp);//register the buttons
-            logIn = (Button) findViewById(R.id.logIn);
-            signUp.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    //when the button is clicked, kick them off to the web browser at the
-                    //specified url
-                    Intent browserIntent = new Intent(getApplicationContext(), Browser.class);
-                    browserIntent.putExtra("url", "https://mixxertestdev.dickinson.edu/user/register");
-                    startActivity(browserIntent);
-                }
-            });
-            logIn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent browserIntent = new Intent(getApplicationContext(), Browser.class);
-                    browserIntent.putExtra("url", "https://mixxertestdev.dickinson.edu/user/login");
-                    startActivity(browserIntent);
-                }
-            });
-    }
+        signUp = (Button) findViewById(R.id.signUp);//register the buttons
+        logIn = (Button) findViewById(R.id.logIn);
+//            signUp.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    //when the button is clicked, kick them off to the web browser at the
+//                    //specified url
+////                    Intent browserIntent = new Intent(getApplicationContext(), Browser.class);
+////                    browserIntent.putExtra("url", "https://www.language-exchanges.org/user/register");
+////                    startActivity(browserIntent);
+//                    String url = "https://www.language-exchanges.org/user/login";
+//                    CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+//                    CustomTabsIntent customTabsIntent = builder.build();
+//                    customTabsIntent.launchUrl(getParent(), Uri.parse(url));
+//                }
+//            });
+//            logIn.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    Intent browserIntent = new Intent(getApplicationContext(), Browser.class);
+//                    browserIntent.putExtra("url", "https://www.language-exchanges.org/user/login");
+//                    startActivity(browserIntent);
+//                }
+//            });
 
-    /**
-     * I was trying to get the login information of the current user so that
-     * the app doesnt ask for it every time they open the app. but we don't need it anymore
-     * since the cache is saved for a year or so. might become useful later, tho.
-     */
-//    private class yourDataTask extends AsyncTask<String, String, String>
-//    {
-//        protected void onPreExecute() {
-//            super.onPreExecute();
-//
-//            pd = new ProgressDialog(MainActivity.this);
-//            pd.setMessage("Please wait");
-//            pd.setCancelable(false);
-//            pd.show();
-//        }
-//
-//        protected String doInBackground(String... params) {
-//
-//
-//            HttpURLConnection connection = null;
-//            BufferedReader reader = null;
-//
-//            try {
-//                URL url = new URL("https://mixxertestdev.dickinson.edu/current_user?_format=json");
-//                connection = (HttpURLConnection) url.openConnection();
-//                connection.connect();
-//
-//
-//                InputStream stream = connection.getInputStream();
-//
-//                reader = new BufferedReader(new InputStreamReader(stream));
-//
-//                StringBuffer buffer = new StringBuffer();
-//                String line = "";
-//
-//                while ((line = reader.readLine()) != null) {
-//                    buffer.append(line+"\n");
-//                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
-//
-//                }
-//                joReturn= buffer.toString();
-//                return buffer.toString();
-//
-//
-//            } catch (MalformedURLException e) {
-//                e.printStackTrace();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            } finally {
-//                if (connection != null) {
-//                    connection.disconnect();
-//                }
-//                try {
-//                    if (reader != null) {
-//                        reader.close();
-//                    }
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//            return null;
-//        }
-//
-//        @Override
-//        protected void onPostExecute(String result) {
-//            super.onPostExecute(result);
-//            if (pd.isShowing()){
-//                pd.dismiss();
-//            }
-//        }
-//    }
+        CustomTabsServiceConnection connection = new CustomTabsServiceConnection() {
+            @Override
+            public void onCustomTabsServiceConnected(ComponentName name, CustomTabsClient client) {
+                mCustomTabsClient = client;
+                boolean ok= mCustomTabsClient.warmup(0); //this warms up the URL trying to load
+                mCustomTabsSession = mCustomTabsClient.newSession(null);
+                List<URL> likelyList= new ArrayList<>();
+                Uri uri= Uri.parse("https://www.language-exchanges.org/user/login"); //uri is the most URL that the
+                //user is most likely to visit
+                mCustomTabsSession.mayLaunchUrl(uri, null, null);// we are telling the custom tab
+                // that the URL above may have the highest chance of being launched
+                // so that chrome tab connects to the URL before the button is actually clicked.
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                mCustomTabsClient = null;
+            }
+        };
+        boolean ok = CustomTabsClient.bindCustomTabsService(this, CUSTOM_TAB_PACKAGE_NAME, connection);
+    }
+    public void launchLogIn(View view){
+        String url = "https://www.language-exchanges.org/user/login";
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        builder.setToolbarColor(Color.parseColor("#263038"));
+        CustomTabsIntent customTabsIntent = builder.build();
+        customTabsIntent.launchUrl(this, Uri.parse(url));
+    }
+    public void launchSignup(View view){
+        String url = "https://www.language-exchanges.org/user/register";
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        CustomTabsIntent customTabsIntent = builder.build();
+        customTabsIntent.launchUrl(this, Uri.parse(url));
+    }
 }
